46f586e2efb910010d28fdc85ced4ca57e5c65a8
Rani Singh <rana_singh32000@yahoo.com>
2019-08-10 00:03:15 -0400

    first commit

---
+++ AssignmentGraphQL 2/AssignmentGraphQL/Models/GithubUserResponse.cs
@@ -0,0 +1,22 @@
+﻿
+
+using System;
+using System.Collections.Generic;
+
+using System.Globalization;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+
+namespace AssignmentGraphQL.Models
+{
+
+    public class GithubUserResponse
+    {
+      
+        public string title { get; set; }
+        public string director { get; set; }
+    }
+
+  
+}
